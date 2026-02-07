cead7e1968ab704dac819fea597cdeab41032203
dinellipaulo <pgsd1453@gmail.com>
2026-02-07 15:53:51 -0400

    criando usuários no banco

--- src/database/index.js
+++ src/database/index.js
@@ -5,8 +5,9 @@ dotenv.config();
 import Sequelize from "sequelize";
 import databaseConfig from '../config/database';
 import Aluno from '../models/Aluno';
+import User from '../models/User';
 
-const models = [Aluno];
+const models = [Aluno, User];
 const connection = new Sequelize(process.env.DATABASE, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {dialect: 'mysql'});
 
 models.forEach(model => model.init(connection));
