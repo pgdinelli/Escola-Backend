6a53512302412413c3bf3457ed1136084d0f46a6
dinellipaulo <pgsd1453@gmail.com>
2026-02-07 19:15:26 -0400

    CRUD de usuários do sistema

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,13 +1,68 @@
 import User from "../models/User";
 
 class UserController {
+    // store/create -> cria um novo usuário -> POST
     async store(req, res) {
         try {
-            const novoUser = await User.create(req.body);
-            res.json(novoUser);
+            const newUser = await User.create(req.body);
+            return res.json(newUser);
 
         } catch (e) {
-            res.status(400).json({
+            return res.status(400).json({
+                    errors: e.errors.map(err => err.message),
+                });
+        }
+    }
+
+    // index -> lista todos os usuários -> GET
+    async index(req, res) {
+        try {
+            const users = await User.findAll();
+            return res.json(users);
+        } catch (e) {
+            return res.json(null);
+        }
+    }
+
+    // show -> mostra um usuário -> GET
+    async show(req, res) {
+        try {
+            const user = await User.findByPk(req.params.id);
+            return res.json(user);
+        } catch (e) {
+            return res.json(null);
+        }
+    }
+
+    // update -> atualiza um usuário -> PATCH ou PUT
+    async update(req, res) {
+        try {
+            if(!req.params.id) return res.status(400).json({errors: ['ID not found.']});
+            const user = await User.findByPk(req.params.id);
+
+            if(!user) return res.status(400).json({ errors: ['User not found']});
+            const newData = await user.update(req.body);
+
+            return res.json(newData);
+        } catch (e) {
+            return res.status(400).json({
+                errors: e.errors.map(err => err.message),
+            });
+        }
+    }
+
+    // delete -> apaga um usuário -> DELETE
+    async delete(req, res) {
+        try {
+            if(!req.params.id) return res.status(400).json({errors: ['ID not found.']});
+            const user = await User.findByPk(req.params.id);
+
+            if(!user) return res.status(400).json({ errors: ['User not found']});
+            await user.destroy(req.body);
+
+            return res.json(user);
+        } catch (e) {
+            return res.status(400).json({
                 errors: e.errors.map(err => err.message),
             });
         }
