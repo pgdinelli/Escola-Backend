1bdac9fffe7e97ca9255225021f90c50b4e78f6b
dinellipaulo <pgsd1453@gmail.com>
2026-03-16 15:20:39 -0400

    salvando fotos no banco de dados

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,16 +1,28 @@
 import multer from 'multer';
 import multerConfig from '../config/multerConfig';
 
+import Foto from '../models/Foto';
+
 const upload = multer(multerConfig).single('photo');
 
 class FotoController {
-    async store(req, res) {
-        return upload(req, res, (err) => {
-            if(err) return res.status(400).json({
+    store(req, res) {
+        return upload(req, res, async (err) => {
+            if (err) return res.status(400).json({
                 errors: [err.code],
             });
 
-            return res.json(req.file);
+            const { originalname, filename } = req.file;
+            const { aluno_id } = req.body;
+
+            try {
+                const photo = await Foto.create({ originalname, filename, aluno_id });
+                return res.json(photo);
+            } catch (error) {
+                return res.status(400).json('Aluno ID does not exist.')
+            }
+
+
         });
     }
 }
--- src/database/index.js
+++ src/database/index.js
@@ -6,8 +6,10 @@ import Sequelize from "sequelize";
 import databaseConfig from '../config/database';
 import Aluno from '../models/Aluno';
 import User from '../models/User';
+import Foto from '../models/Foto';
 
-const models = [Aluno, User];
+const models = [Aluno, User, Foto];
 const connection = new Sequelize(process.env.DATABASE, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {dialect: 'mysql'});
 
 models.forEach(model => model.init(connection));
+models.forEach(model => model.associate && model.associate(connection.models));
